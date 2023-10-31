f23a739a849b41706fe7d532dd7e780b67ae9d4f
Tamanna14-cpu <bimurto.bithy14@gmail.com>
2023-11-01 01:41:40 +0600

    [done] image layout responsive

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import Header from "./components/Header";
-import ImgLayout from "./components/ImgLayout";
+import ImgLayout from "./components/imgLayout/ImgLayout";
 
 function App() {
   return (
---
+++ src/components/imgLayout/ImgLayout.jsx
@@ -0,0 +1,62 @@
+import { useState } from "react";
+import imgData from "../../utils/imgData.json";
+import "./ImgLayout.css";
+import featureImg from "/images/image-11.jpeg";
+
+const ImgLayout = () => {
+  const [selectedImages, setSelectedImages] = useState([]);
+  const [hoveredImage, setHoveredImage] = useState(null);
+
+  console.log(hoveredImage);
+
+  // Function to toggle the selection of an image
+  const toggleSelection = (id) => {
+    if (selectedImages.includes(id)) {
+      setSelectedImages(
+        selectedImages.filter((selectedId) => selectedId !== id)
+      );
+    } else {
+      setSelectedImages([...selectedImages, id]);
+    }
+  };
+
+  return (
+    <div className="grid grid-cols-5 grid-flow-row gap-8 child-wrapper pb-10">
+      <div className="row-span-2 col-span-2 image-border">
+        <img src={featureImg} alt="featureImage" />
+      </div>
+
+      {imgData?.map(({ id, imgSrc }) => {
+        const isSelected = selectedImages.includes(id);
+        const isHovered = hoveredImage === id;
+
+        return (
+          <div key={id} className="relative">
+            <div
+              className={`col-span-1 image-border ${
+                isSelected ? "selected" : ""
+              }`}
+              onClick={() => toggleSelection(id)}
+              onMouseEnter={() => setHoveredImage(id)}
+              onMouseLeave={() => setHoveredImage(null)}
+            >
+              <img src={imgSrc} alt="gallery" />
+
+              {isHovered && (
+                <label className="checkbox-label">
+                  <input
+                    type="checkbox"
+                    checked={isSelected}
+                    onChange={() => toggleSelection(id)}
+                  />
+                </label>
+              )}
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default ImgLayout;
