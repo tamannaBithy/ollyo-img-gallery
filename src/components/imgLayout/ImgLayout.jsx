87ad7cb3807c112395b461eec37baf8e3b5ec771
Tamanna14-cpu <bimurto.bithy14@gmail.com>
2023-11-01 15:29:40 +0600

    [done] img can be selected

--- src/components/imgLayout/ImgLayout.jsx
+++ src/components/imgLayout/ImgLayout.jsx
@@ -1,13 +1,10 @@
 import { useState } from "react";
 import imgData from "../../utils/imgData.json";
 import "./ImgLayout.css";
-import featureImg from "/images/image-11.jpeg";
 
 const ImgLayout = () => {
   const [selectedImages, setSelectedImages] = useState([]);
   const [hoveredImage, setHoveredImage] = useState(null);
-
-  console.log(selectedImages);
 
   // Function to toggle the selection of an image
   const toggleSelection = (id) => {
@@ -21,37 +18,32 @@ const ImgLayout = () => {
   };
 
   return (
-    <div className="grid lg:grid-cols-5 grid-cols-3 grid-flow-row gap-8 child-wrapper pb-10">
-      <div className="row-span-2 col-span-2 image-border hover:brightness-50">
-        <img src={featureImg} alt="featureImage" />
-      </div>
-
+    <div className=" grid lg:grid-cols-5 grid-cols-3 grid-flow-row gap-8 child-wrapper pb-10">
       {imgData?.map(({ id, imgSrc }) => {
         const isSelected = selectedImages.includes(id);
         const isHovered = hoveredImage === id;
 
         return (
-          <div key={id} className="relative">
-            <div
-              className={`col-span-1 image-border ${
-                isSelected ? "selected" : ""
-              }`}
-              onClick={() => toggleSelection(id)}
-              onMouseEnter={() => setHoveredImage(id)}
-              onMouseLeave={() => setHoveredImage(null)}
-            >
-              <img src={imgSrc} alt="gallery" className="hover:brightness-50" />
+          <div
+            key={id}
+            className={`relative image-border ${
+              id === 1 ? "row-span-2 col-span-2" : "col-span-1"
+            }`}
+            onClick={() => toggleSelection(id)}
+            onMouseEnter={() => setHoveredImage(id)}
+            onMouseLeave={() => setHoveredImage(null)}
+          >
+            <img src={imgSrc} alt="gallery" className="hover:brightness-50" />
 
-              {(isSelected || isHovered) && (
-                <label className="checkbox-label">
-                  <input
-                    type="checkbox"
-                    checked={isSelected}
-                    onChange={() => toggleSelection(id)}
-                  />
-                </label>
-              )}
-            </div>
+            {(isSelected || isHovered) && (
+              <label className="checkbox-label">
+                <input
+                  type="checkbox"
+                  checked={isSelected}
+                  onChange={() => toggleSelection(id)}
+                />
+              </label>
+            )}
           </div>
         );
       })}
